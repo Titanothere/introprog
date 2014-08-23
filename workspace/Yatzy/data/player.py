73cfce21eb8647dbdcfb3290d5130e5acea7ef1e
Ygge95 <ygge95@gmail.com>
2014-08-23 20:39:52 +0000

    xrange

---
+++ workspace/Yatzy/data/player.py
@@ -0,0 +1,47 @@
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.points = {
+        "1"                : 0,
+        "2"                : 0,
+        "3"                : 0,
+        "4"                : 0,
+        "5"                : 0,
+        "6"                : 0,
+        "pair"             : 0,
+        "two pairs"        : 0,
+        "three of a kind"  : 0,
+        "four of a kind"   : 0,
+        "full house"       : 0,
+        "small straight"   : 0,
+        "large straight"   : 0,
+        "yatzee"           : 0,
+        "chance"           : 0
+        }
+        self.checklist = ["1",
+                          "2",
+                          "3",
+                          "4",
+                          "5",
+                          "6",
+                          "pair",
+                          "two pairs",
+                          "three of a kind",
+                          "four of a kind",
+                          "full house",
+                          "small straight", 
+                          "large straight",
+                          "yahtzee",
+                          "chance"]
+    def get_score_total(self):
+        s = 0
+        subtotal = 0
+        for p in self.points.values():
+            s += p
+        for r in xrange(1, 7):
+            subtotal += self.points[str(r)]
+        if r >= 63:
+            s += 50
+        return s
+        
---
+++ workspace/Yatzy/main.py
@@ -0,0 +1,4 @@
+from data.game import *
+
+if __name__ == "__main__"
+  game()
